2b6e235afb3f9b83299d060cc494f983c1999164
ddandipu <andiputra.dimas@hotmail.com>
2017-05-04 22:07:12 +0000

    loaded die exercise

---
+++ loadedDieGenerator.js
@@ -0,0 +1,37 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4];
+  /* your code here */
+  var occurrence = -1;
+  return function() {
+    if (occurrence > list.length-2) {
+      occurrence = -1}
+      /* your code here */
+    occurrence += 1;
+    return list[occurrence];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
+console.log(rollLoadedDie());
